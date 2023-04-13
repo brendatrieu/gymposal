d303b59efcb9430fcc8894b8a1c88b347a2a05c7
Brenda Trieu <trieubrenda@gmail.com>
2023-04-13 06:12:36 +0000

    Began working on launch pad and nav bar.

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,53 @@
-import { useEffect, useState } from 'react';
-import logo from './logo.svg';
+// import { useEffect, useState } from 'react';
+import { createTheme, ThemeProvider } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
 import './App.css';
+import Header from './components/Header';
+
 
 function App() {
-  const [serverData, setServerData] = useState("");
+  const theme = createTheme({
+    palette: {
+      mode: "dark",
+      primary: {
+        main: '#ffffff'
+      },
+      secondary: {
+        main: '#151a26'
+      },
+    },
+    typography: {
+      fontFamily: 'Open Sans',
+      button: {
+        fontSize: '0.05rem',
+        textTransform: 'unset'
+      }
+    },
+  })
+  // const [serverData, setServerData] = useState("");
 
-  useEffect(() => {
-    async function getServerData() {
-      const resp = await fetch('/api/hello');
-      const data = await resp.json();
+  // useEffect(() => {
+  //   async function getServerData() {
+  //     const resp = await fetch('/api/hello');
+  //     const data = await resp.json();
 
-      console.log('Data from server:', data);
+  //     console.log('Data from server:', data);
 
-      setServerData(data.message);
-    }
+  //     setServerData(data.message);
+  //   }
 
-    getServerData();
-  }, []);
+  //   getServerData();
+  // }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>{serverData}</h1>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header />
+    </ThemeProvider>
+    // <CssVarsProvider>
+    //   <CssBaseline />
+      /* <ResponsiveAppBar /> */
+    // </CssVarsProvider>
   );
 }
 
---
+++ client/src/components/Header.js
@@ -0,0 +1,22 @@
+import { AppBar, Toolbar, Typography, Box, IconButton, Button } from '@mui/material';
+import LogoutIcon from '@mui/icons-material/Logout';
+
+export default function Header() {
+
+  return (
+    <AppBar sx={{ flexGrow: 1 }} position="fixed" color="transparent">
+      <Toolbar sx={{ justifyContent: "space-between"}}>
+        <IconButton>
+          <Typography variant="h5" sx={{ fontFamily: "'Permanent Marker', cursive" }}>
+            GYMPOSAL
+          </Typography>
+        </IconButton>
+        <Box sx={{ display: 'flex' }}>
+          <Button size="small">Log Exercise</Button>
+          <Button size="small">Create Group</Button>
+          <IconButton><LogoutIcon /></IconButton>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+}
