af31515c8be732155e590881a130cdac538ebbcc
Brenda Trieu <trieubrenda@gmail.com>
2023-06-07 21:42:34 +0000

    Updated styling to for exercise info section.

--- client/src/components/ExerciseInfo.js
+++ client/src/components/ExerciseInfo.js
@@ -1,48 +1,44 @@
 import { Grid, Typography } from '@mui/material';
+import BaseGraph from './BaseGraph';
 
 export default function ExerciseInfo() {
   const data = [
     { date: '01/01/2023', type: 'Running', totalMinutes: 45 },
-    { date: '01/03/2023', type: 'Yoga', totalMinutes: 60 },
-    { date: '01/04/2023', type: 'Lift Weights', totalMinutes: 30 },
-    { date: '01/02/2023', type: 'Hiking', totalMinutes: 90 },
+    { date: '01/02/2023', type: 'Yoga', totalMinutes: 60 },
+    { date: '01/03/2023', type: 'Lift Weights', totalMinutes: 30 },
+    { date: '01/04/2023', type: 'Hiking', totalMinutes: 90 },
     { date: '01/05/2023', type: 'H.I.I.T.', totalMinutes: 45 },
   ];
 
   return (
-    <Grid container justifyContent="space-evenly" >
-      <Grid item xs={12} sm={8} md={5}>
-        <img
-          style={{width: '100%' }}
-          src='./graph_example.png'
-          alt="A graph showing total minutes of exercise per day."
-        />
+    <Grid container my={2} justifyContent="space-between" >
+      <Grid item xs={12} sm={8} md={6} sx={{paddingX: 4, minHeight: '35vh' }} >
+        <BaseGraph exercises={data} />
       </Grid>
       <Grid
         item
         container
-        xs={12} sm={4} md={5}
+        xs={12} sm={4} md={6}
         sx={{ padding: 4,
           justifyContent: 'space-evenly',
           flexDirection: 'column',
           display: 'flex' }}
       >
-        <Grid mr={8}>
+        <Grid mr={16}>
           <Typography variant="h6" align="left">
             <strong>
               Log your exercises to begin generating data.
             </strong>
           </Typography>
         </Grid>
-        <Grid mt={3} ml={8}>
+        <Grid mt={3} ml={16}>
           <Typography variant="h6" align="right">
             <strong>
-              View your weekly progress and see your improvements.
+              View your weekly progress & see your improvements.
             </strong>
           </Typography>
         </Grid>
       </Grid>
     </Grid>
-
   )
 }
